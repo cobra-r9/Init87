5fae6348c131ea1d6b6f299c80ba5ee497a35ab9
cobra-r9 <cobra.rev.9@gmail.com>
2026-05-08 10:33:27 +0530

    add; w3schools stuff for practice;

---
+++ misc/w3schools/many_printfs.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World!\n");
+  printf("Have a good day!\n");
+  return 0;
+}
---
+++ misc/w3schools/printf_no_newline.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("hello world;");
+  printf("this is a second statement;");
+  printf("notice, no newline!\n");
+  return 0;
+}
